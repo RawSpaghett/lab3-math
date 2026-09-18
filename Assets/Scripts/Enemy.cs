54eb5f67cac7cf994c4a544eaad23f27a091ce57
RawSpaghett <rawspaghetti05@gmail.com>
2026-09-18 15:10:35 -0400

    rotation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
+
 
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player"); 
     }
 
     void FixedUpdate()
     {
-        
+        Vector2 direction = player.transform.position - transform.position; //Step 1: gets direction 
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f; //Step 2: Calculate angle in radians via rise-over-run with Atan, and calculate back to degrees for actual transformation, subtract 90 degrees for the sprite
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle); //Step 3:  Apply transformation
+
     }
 }
