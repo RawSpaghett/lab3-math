4f92e1347a02fbee065c68b29529b13a60b3ad1b
drewp03 <drewprato03@gmail.com>
2026-09-20 17:39:14 -0400

    added enemy orbit functionality

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,19 +4,57 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject player;
 
+    [Header("Orbit Settings")]
+    [SerializeField] private float radius = 5f;
+    [SerializeField] private float orbitSpeed = 2f;
+
+    private float orbitAngle;
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player"); 
+        Init();
     }
 
     void FixedUpdate()
+    {
+        LookAt();
+        Circle();
+    }
+
+    private void LookAt()
     {
         Vector2 direction = player.transform.position - transform.position; //Step 1: gets direction 
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f; //Step 2: Calculate angle in radians via rise-over-run with Atan, and calculate back to degrees for actual transformation, subtract 90 degrees for the sprite
 
         transform.rotation = Quaternion.Euler(0f, 0f, angle); //Step 3:  Apply transformation
+    }
 
+    private void Circle()
+    {
+        //increases the angle over time
+        orbitAngle += orbitSpeed * Time.deltaTime;
+        
+        //finding the x and y position every call, cosine tells us the x position just by having the angle, and sin does the same for y
+        float x = Mathf.Cos(orbitAngle) * radius;
+        float y = Mathf.Sin(orbitAngle) * radius;
+
+        Vector3 newPosition = new Vector3(x,y,0);
+
+        transform.position = newPosition;
+    }
+
+    private void Init()
+    {
+        player = GameObject.FindWithTag("Player"); 
+
+        //calculate enemy distance relative to player
+        Vector3 enemyPos = transform.position - player.transform.position;
+
+        //takes the enemyPosition offset and returns a radian that tell us the starting angle that the enemy will begin to rotate around us from
+        orbitAngle = Mathf.Atan2(enemyPos.y,enemyPos.x);
+
+        radius = Random.Range(2f,6f);
+        orbitSpeed = Random.Range(1f,3f);
     }
 }
